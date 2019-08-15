a537ce3d950202af63204e1e323af09395e8daa2
narsimha-na <activena1@gmail.com>
2019-08-15 12:29:44 +0530

    Project Setup

--- app/src/main/java/com/example/nanews/MainActivity.java
+++ app/src/main/java/com/example/nanews/MainActivity.java
@@ -2,16 +2,59 @@ package com.example.nanews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.nanews.auth.Authentication;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth maAuth;
+    private ImageView img;
+    private TextView name,email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        maAuth = FirebaseAuth.getInstance();
+
+        name = (TextView) findViewById(R.id.name);
+        email = (TextView)findViewById(R.id.email);
+        img = (ImageView)findViewById(R.id.img);
 
 
+        FirebaseUser fbUser = maAuth.getCurrentUser();
+
+        try{
+            Glide.with(this).load(fbUser.getPhotoUrl()).into(img);
+            name.setText(fbUser.getDisplayName());
+            email.setText(fbUser.getEmail());
+        }catch (Exception e){
+            Toast.makeText(MainActivity.this,"Failed Image",Toast.LENGTH_LONG).show();
+        }
+
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startActivity(new Intent(MainActivity.this, Authentication.class));
     }
 }
--- app/src/main/java/com/example/nanews/auth/StartUpFragment.java
+++ app/src/main/java/com/example/nanews/auth/StartUpFragment.java
@@ -1,22 +1,46 @@
 package com.example.nanews.auth;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.nanews.MainActivity;
 import com.example.nanews.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Objects;
+import java.util.concurrent.Executor;
+
+import static android.content.ContentValues.TAG;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class StartUpFragment extends Fragment {
 
+    private static final int RC_SIGN_IN = 247;
+    private View sufView;
+    private FirebaseAuth sufAuth;
+    private GoogleSignInClient sufGoogle;
 
     public StartUpFragment() {
         // Required empty public constructor
@@ -26,8 +50,84 @@ public class StartUpFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_up, container, false);
+        sufView = inflater.inflate(R.layout.fragment_start_up, container, false);
+        //first we intialized the FirebaseAuth object
+        sufAuth = FirebaseAuth.getInstance();
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        //Then we will get the GoogleSignInClient object from GoogleSignIn class
+        sufGoogle = GoogleSignIn.getClient(Objects.requireNonNull(getContext()),gso);
+
+        sufView.findViewById(R.id.signInButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
+        return sufView;
     }
 
+    private void signIn() {
+        Intent signInIntent = sufGoogle.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        sufAuth.signInWithCredential(credential)
+                .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            Toast.makeText(getContext(), "User Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = sufAuth.getCurrentUser();
+                            startActivity(new Intent(getContext(), MainActivity.class));
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getContext(), "User Logging Failed", Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(!(sufAuth.getCurrentUser() == null)){
+            startActivity(new Intent(getContext(), MainActivity.class));
+        }
+    }
 }
